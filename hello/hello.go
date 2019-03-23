92be0f249cafa36c69aa5995abae66f7f55679ea
Thomas Baird <tmobaird@gmail.com>
2019-03-23 11:16:34 -0400

    Adding languages

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,36 @@ package main
 import "fmt"
 
 const englishPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishPrefix = "Hola, "
+const french = "French"
+const frenchPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+/* Note:
+ * Named Return Values
+ * `(prefix string)` will initialize a string variable to "" named prefix in the function
+ * the empty return at the end will auto return the "named return value"
+ */
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchPrefix
+	case spanish:
+		prefix = spanishPrefix
+	default:
+		prefix = englishPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,15 +12,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say Hello, World when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Jacques", "French")
+		want := "Bonjour, Jacques"
 
 		assertCorrectMessage(t, got, want)
 	})
@@ -29,7 +43,7 @@ func TestHello(t *testing.T) {
 // You can have as many tests as you want in a go test file
 // The function name must be TestXXX with the same signature
 func TestHello2(t *testing.T) {
-	got := Hello("Thomas")
+	got := Hello("Thomas", "")
 	want := "Hello, Thomas"
 
 	if got != want {
